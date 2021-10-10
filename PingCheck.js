f9a98ae4dc7f59625e119fbf880c425a42c16ee3
Pinto565 <intoinfant5650@gmail.com>
2021-10-10 23:19:27 +0530

    Initial Commit

---
+++ PingCheck.js
@@ -0,0 +1,44 @@
+const shell = require("shelljs");
+const date = require("date-and-time");
+
+const final_op = {
+  device_status: [],
+  update_time: "",
+};
+
+const ping_schema = (ip_add, status) => {
+  const ping_json = {
+    host: ip_add,
+    status: status,
+  };
+  return ping_json;
+};
+
+const ping_function = (ip_addr) => {
+  var ping = shell.exec(`ping -n 1 ${ip_addr}`, {
+    silent: true,
+  });
+  if (ping.stdout.includes("host unreachable")) {
+    final_op["device_status"].push(ping_schema(ip_addr, "Offline"));
+  } else if (ping.stdout.includes("timed out")) {
+    final_op["device_status"].push(ping_schema(ip_addr, "Offline"));
+  } else {
+    final_op["device_status"].push(ping_schema(ip_addr, "Online"));
+  }
+};
+
+const ping_devices = () => {
+  const now = new Date();
+  final_op.device_status = [];
+  final_op.update_time = date.format(now, "YYYY/MM/DD HH:mm:ss");
+  const devices =
+    "192.168.1.100 192.168.1.102 vpn.opencloud.pattarai.in pattarai.in pintoinfant.tech".split(
+      " "
+    );
+  devices.forEach((device) => {
+    ping_function(device);
+  });
+  return final_op;
+};
+
+module.exports = ping_devices;
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const ping = require("./PingCheck");
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json(ping());
+});
+
+app.listen(PORT, () => {
+  console.log(`App is Running on Port ${PORT}`);
+});
