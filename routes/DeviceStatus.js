f6c63c534c9fcc0db14af5ec52dccba5c1b13f35
Pinto565 <pintoinfant5650@gmail.com>
2021-12-21 11:16:12 +0530

    file renamed

--- routes/DeviceStatus.js
+++ routes/DeviceStatus.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const final_op = {
   device_status: [],
-  update_time: "",
+  updated_time: "",
 };
 
 const ping_schema = (ip_add, status) => {
@@ -16,17 +16,16 @@ const ping_schema = (ip_add, status) => {
 };
 
 const ping_function = (ip_addr) => {
-  var ping = shell.exec(`ping -n 1 ${ip_addr}`, {
+  let ping = shell.exec(`ping -n 1 ${ip_addr}`, {
     silent: true,
-  });
-  // console.log(ping.stdout);
-  if (ping.stdout.includes("Request timed out")) {
+  }).stdout;
+  if (ping.includes("Request timed out")) {
     final_op["device_status"].push(ping_schema(ip_addr, "Unavailable"));
-  } else if (ping.stdout.includes("Destination host unreachable")) {
+  } else if (ping.includes("Destination host unreachable")) {
     final_op["device_status"].push(ping_schema(ip_addr, "Offline"));
-  } else if (ping.stdout.includes("100% packet loss")) {
+  } else if (ping.includes("100% packet loss")) {
     final_op["device_status"].push(ping_schema(ip_addr, "Offline"));
-  } else if (ping.stdout.includes("could not find host")) {
+  } else if (ping.includes("could not find host")) {
     final_op["device_status"].push(ping_schema(ip_addr, "Unavailable"));
   } else {
     final_op["device_status"].push(ping_schema(ip_addr, "Online"));
@@ -36,10 +35,10 @@ const ping_function = (ip_addr) => {
 const ping_devices = () => {
   const now = new Date();
   final_op.device_status = [];
-  final_op.update_time = date.format(now, "YYYY/MM/DD HH:mm:ss");
+  final_op.updated_time = date.format(now, "YYYY/MM/DD HH:mm:ss");
   const devices = (
     process.env.DEVICES ||
-    "pintoinfant.tech 127.0.0.1"
+    "pintoinfant.tech 127.0.0.1 opencloud.pattarai.in"
   ).split(" ");
   devices.forEach((device) => {
     ping_function(device);
