93ea79f80961605b3c2a770746a375c4e5831325
Pinto565 <pintoinfant5650@gmail.com>
2022-01-24 10:55:01 +0530

    python setup

--- setup.sh
+++ setup.sh
@@ -6,3 +6,11 @@ apt update -y
 #Installing Nodejs and NPM
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt install nodejs -y
+
+#Installing Python and Virtualenv
+apt install software-properties-common -y
+add-apt-repository ppa:deadsnakes/ppa -y
+apt update -y
+apt install python3.8 -y
+apt-get install python3-pip -y
+pip3 install virtualenv -y
